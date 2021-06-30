7659175fc210680dd3237b722323b1c321685f01
Chrisw <chris_white_12@hotmail.com>
2021-06-30 19:57:03 +1000

    fixing footer icons, pointer hover over video link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
+import * as Icons from 'react-bootstrap-icons'
+
 const Footer = () => {
     return (
         <footer>
             <div className="logobar">
-                <li> <a target='_blank' rel='noreferrer' href="https://github.com/ChrisWhite12"><img src="./GitHub-Icon-01.svg" alt="GitHub" /></a></li>  
-                <li> <a target='_blank' rel='noreferrer' href="https://www.linkedin.com/in/chriswhite16/"><img src="./Linkedin-04.svg" alt="Linkedin" /></a></li>
+                <a target='_blank' rel='noreferrer' href="./Resume.pdf"><Icons.FileEarmarkPersonFill color='#d6f2f8' size={25} /></a>
+                <a target='_blank' rel='noreferrer' href="https://github.com/ChrisWhite12"><Icons.Github color='#d6f2f8' size={25} /></a>
+                <a target='_blank' rel='noreferrer' href="https://www.linkedin.com/in/chriswhite16/"><Icons.Linkedin color='#d6f2f8' size={25} /></a>
+                <a target='_blank' rel='noreferrer' href='mailto:chris_white_12@hotmail.com'><Icons.EnvelopeFill color='#d6f2f8' size={25} /></a>
             </div>
+                
+                
+                
+                
         </footer>
     )
 }
